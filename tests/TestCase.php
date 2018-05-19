a578cc076fdd64f69e0bfef0f044ad1400754c4a
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2018-05-19 19:51:16 -0400

    Add helper method to get response data for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,25 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @param TestResponse $response
+     * @param null|String $key
+     * @return mixed
+     */
+    public function getResponseData(TestResponse $response, ?String $key)
+    {
+        $data = $response->getOriginalContent()->getData();
+
+        if ($key) {
+            return array_get($data, $key);
+        }
+
+        return $data;
+    }
 }

c7908c4b18572b00ecf225c2810a4c431b33ec31
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2018-05-21 22:00:33 -0400

    Create buttons partial

---
+++ resources/views/reports/_buttons.blade.php
@@ -0,0 +1,5 @@
+<a href="{{ route('reports.index') }}">
+    <button type="button" class="btn pull-left">Cancel</button>
+</a>
+
+<button type="submit" class="btn btn-primary pull-right">Save</button>

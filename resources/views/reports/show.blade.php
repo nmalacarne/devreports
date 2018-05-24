e82ea7c05500fb747fe7177bb594de2832eb0435
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2018-05-24 16:52:52 -0400

    Fix reports markup
    I missed these when upgrading bootstrap. ¯\_(ツ)_/¯

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-       <div class="col-lg-6 col-lg-offset-3">
-           <div class="panel panel-info">
-               <div class="panel-heading">
-                   <div class="panel-title">
-                       View Report
-                   </div>
+    <div class="row justify-content-center">
+       <div class="col-lg-6">
+           <div class="card">
+               <div class="card-header">
+                   View Report
                </div>
 
-               <div class="panel-body">
+               <div class="card-body">
                    <div class="form-group">
                        <label for="progress" class="control-label">Progress</label>
                        <textarea name="progress" class="form-control" disabled>{{ $report->progress }}</textarea>

2c12a73e2dfafbed946c9e9dc7ebc9e3cb4865be
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2018-05-24 16:48:34 -0400

    Refactor reports show partial
    It is easier to use the fields partial for just create/update.

--- resources/views/reports/_fields.blade.php
+++ resources/views/reports/_fields.blade.php
@@ -1,10 +1,6 @@
 <div class="form-group {{ $errors->has('progress') ? 'has-error' : '' }}">
    <label for="progress" class="control-label">Progress</label>
-   @if (request()->route()->getName() === 'reports.show')
-       <textarea name="progress" class="form-control" disabled>{{ $report->progress }}</textarea>
-   @else
-       <textarea name="progress" class="form-control">{{ $report->progress }}</textarea>
-   @endif
+   <textarea name="progress" class="form-control">{{ $report->progress }}</textarea>
 
    @if ($errors->has('progress'))
        <span class="help-block">{{ $errors->first('progress') }}</span>

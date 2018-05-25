9e7148aff28ce22fc3622612b30c85e41c0f51dc
Nicholas Malacarne <nicholas.malacarne@gmail.com>
2018-05-25 17:25:47 -0400

    Tweak home markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-3">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">My Reports</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <a href="{{ route('reports.index') }}" class="hover-card-body">
+                    <div class="card-body text-center">
+                        <h1>{{ auth()->user()->reports()->count() }}</h1>
+                    </div>
+                </a>
 
-                    You are logged in!
+                <div class="card-footer text-center">
+                    <a href="{{ route('reports.create') }}">
+                        <button type="button" class="btn btn-primary">Create a Report</button>
+                    </a>
                 </div>
             </div>
         </div>
